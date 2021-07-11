abff2a535d53a93434de4ccb6b5a7c3ab6385b6e
lian <yuri_suki@163.com>
2021-07-11 14:26:20 +0800

    解决 ReturnProduct实体字段乱码

--- crm-back/src/main/java/com/crm/entity/ReturnProduct.java
+++ crm-back/src/main/java/com/crm/entity/ReturnProduct.java
@@ -1,52 +1,29 @@
 package com.crm.entity;
 
+import java.math.BigDecimal;
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
-@Data
 /**
  * (ReturnProduct)实体类
  *
  * @author makejava
- * @since 2021-07-11 11:35:19
+ * @since 2021-07-11 14:24:40
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ReturnProduct implements Serializable {
-    private static final long serialVersionUID = -39108124590508972L;
+    private static final long serialVersionUID = 122397266473173903L;
 
-    private Integer íë»õµ¥²úæ·id
+    private Integer returnProductId;
 
     private Integer proId;
 
     private Integer saleReturnId;
-
-
-    public Integer getÍë»õµ¥²úæ·
-
-    id() {
-        return íë»õµ¥²úæ·id;
-    }
-
-    public void setÍë»õµ¥²úæ·
-
-    id(Integer íë»õµ¥²úæ·id) {
-        this.íë»õµ¥²úæ·id = íë»õµ¥²úæ·id;
-    }
-
-    public Integer getProId() {
-        return proId;
-    }
-
-    public void setProId(Integer proId) {
-        this.proId = proId;
-    }
-
-    public Integer getSaleReturnId() {
-        return saleReturnId;
-    }
-
-    public void setSaleReturnId(Integer saleReturnId) {
-        this.saleReturnId = saleReturnId;
-    }
-
 }
